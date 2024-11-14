85cf8ce8ccf62ce4a227754a3d4aa05ef4baba17
Sofia Enciso <sofiaenciso413@gmail.com>
2024-11-13 19:55:34 -0500

    actualización color y estacionamiento

--- proyecto/settings.py
+++ proyecto/settings.py
@@ -118,7 +118,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
-    BASE_DIR / "assets",  
+    BASE_DIR / "assets¡¡¡",  
 ]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
