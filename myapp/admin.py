6c27da1767b29d3fdba7023828fdc02c478f1f0a
sofiaenciso8 <sofiaenciso413@gmail.com>
2024-11-21 08:51:18 -0500

    Delete myapp directory

--- myapp/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import Parqueadero, Vehiculo, Cliente, Mapa, Usuario
-
-admin.site.register(Parqueadero)
-admin.site.register(Vehiculo)
-admin.site.register(Cliente)
-admin.site.register(Mapa)
-admin.site.register(Usuario)
--- myapp/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-urlpatterns = [
-    path('', views.index_view, name='index'),  
-    path('parqueadero/<int:parqueadero_id>/', views.parqueadero_view, name='parqueadero'),
-    path('usuario/<int:usuario_id>/', views.usuario_view, name='usuario'),  # Esta es tu URL
-    path('vehiculo/<int:vehiculo_id>/', views.vehiculo_view, name='vehiculo'),
-    path('mapa/<int:mapa_id>/', views.mapa_view, name='mapa'),
-    ]
