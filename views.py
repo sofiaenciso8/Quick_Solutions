d76743016a97945468aa8ffda6cdc47af43e3bf8
sofiaenciso8 <sofiaenciso413@gmail.com>
2024-11-21 08:54:18 -0500

    Delete views.py

--- views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def mapa_view(request):
-    estados = {
-        'A1': 1, 'A2': 0, 'A3': 1, 'A4': 0,  # etc.
-    }
-    return render(request, 'miapp/mapa.html', {'estados': estados})
